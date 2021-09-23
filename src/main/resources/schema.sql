743dfc4e5249e9c6df290d5efd747321047c8a39
kaslufl <leallucasfonseca@gmail.com>
2021-09-23 17:03:54 -0300

    config: add schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,53 @@
+create table produto
+(
+    id int primary key,
+    nome varchar(100) not null,
+    descricao varchar(200) not null,
+    fotoUrl varchar(200),
+    dataCadastro timestamp not null,
+    dataUltimaAtualizacao timestamp not null,
+    valorUnitario decimal not null
+);
+
+create table categoria
+(
+    id int primary key,
+    nome varchar(100) not null,
+    descricao varchar(200) not null,
+    imagemSimboloUrl varchar(200)
+);
+
+create table categoriaProduto
+(
+    id int primary key,
+    categoriaId int not null,
+    produtoId int not null,
+
+    foreign key (categoriaId) references categoria(id),
+    foreign key (produtoId) references produto(id)
+);
+
+create table promocao
+(
+    id int primary key,
+    nome varchar(100) not null,
+    descricao varchar(200) not null,
+    dataInicio date not null,
+    dataFim date not null,
+    dataCadastro timestamp not null,
+    dataUltimaAtualizacao timestamp not null
+
+);
+
+create table promocaoItem
+(
+    id int primary key,
+    valorPromocao decimal not null,
+    dataCadastro timestamp not null,
+    dataUltimaAtualizacao timestamp not null,
+    produtoId int not null,
+    promocaoId int not null,
+
+    foreign key (promocaoId) references promocao(id),
+    foreign key (produtoId) references produto(id)
+);
